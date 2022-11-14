cad7001a4f3371200866b1e02bfa49deedc340ad
Andrew <pagin.andrey.98@gmail.com>
2022-11-14 12:33:17 +0600

    code cleanup

--- src/main/java/it/pagin/deeplay/logic/Logic.java
+++ src/main/java/it/pagin/deeplay/logic/Logic.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Set;
 
 public class Logic {
-    Logger logger = LoggerFactory.getLogger(Logic.class);
+    private final Logger logger = LoggerFactory.getLogger(Logic.class);
     private PropertiesModel properties;
 
     public List<Integer> performCalculations(final DataModel animals, final PropertiesModel properties) {
